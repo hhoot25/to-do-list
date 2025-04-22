e8e7a7f32a691db7fec6b5becebb9048597ab24e
hhoot25 <hhoot25@gmail.com>
2025-04-22 16:50:34 -0500

    create to-do js

---
+++ src/create_todos.js
@@ -0,0 +1,12 @@
+//used to create todos
+
+class Todo{
+    constructor(title, description, dueDate, priority){
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+
+
+    }
+}
